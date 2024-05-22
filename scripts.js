1082ff458a64edfe1a1b86b838b592c9cacfc219
bergaminho <126927713+leobergamo10@users.noreply.github.com>
2024-05-22 19:45:07 -0300

    4

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleImage() {
-    var img = document.getElementById("imagem-impacto");
-    if (img.style.display === "none") {
-        img.style.display = "block";
-    } else {
-        img.style.display = "none";
-    }
-}
